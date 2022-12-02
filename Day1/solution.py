e398b307d68b644ba05cba6656c33a9e11af8e72
Kbtech <6333813+Kbtech@users.noreply.github.com>
2022-12-02 13:12:13 -0500

    Day 1 solution

--- Day1/solution.py
+++ Day1/solution.py
@@ -1,15 +1,22 @@
+totalScore = 0
+topElfNums = []
 
-topElfScore = topElfNum = currentElfScore = 0
-currentElfNum = 1
+for i in range(1, 4):
+    topElfScore = topElfNum = currentElfScore = 0
+    currentElfNum = 1
 
-file = open("input.txt", "r")
-for line in file:
-    if line == "\n":
-        if topElfScore < currentElfScore:
-            topElfScore = currentElfScore
-            topElfNum = currentElfNum
-        currentElfScore = 0
-        currentElfNum += 1
-    else:
-        currentElfScore += int(line)
-print("Elf: " + str(topElfNum) + " Score: " + str(topElfScore))
+    file = open("input.txt", "r")
+    for line in file:
+        if line == "\n":
+            if topElfScore < currentElfScore and currentElfNum not in topElfNums:
+                topElfScore = currentElfScore
+                topElfNum = currentElfNum
+            currentElfScore = 0
+            currentElfNum += 1
+        else:
+            currentElfScore += int(line)
+    print("Elf: " + str(topElfNum) + " Score: " + str(topElfScore))
+    
+    totalScore += topElfScore
+    topElfNums.append(topElfNum)
+print("Top 3 Elfs Score: " + str(totalScore))
